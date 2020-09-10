2dfedab394161a5f6857131b9443bb523ada505c
Sebastian Meyne <sebastian.meyne@gmail.com>
2020-09-10 20:39:02 +0200

    initialised components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { MovielistComponent } from './movielist/movielist.component';
+import { CheckoutComponent } from './checkout/checkout.component';
+import { AuthenticationComponent } from './authentication/authentication.component';
+import { CartComponent } from './cart/cart.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    MovielistComponent,
+    CheckoutComponent,
+    AuthenticationComponent,
+    CartComponent
   ],
   imports: [
     BrowserModule,
---
+++ src/app/movielist/movielist.component.ts
@@ -0,0 +1,30 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-movielist',
+  templateUrl: './movielist.component.html',
+  styleUrls: ['./movielist.component.css']
+})
+export class MovielistComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+
+
+  }
+
+
+  
+
+
+
+
+
+
+
+
+
+
+
+}
