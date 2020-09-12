9e2955017964fe86649c90012ef6219065d359d7
Sebastian Meyne <sebastian.meyne@gmail.com>
2020-09-12 19:51:48 +0200

    added methods to backend.service, added wrapper method fetch

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,8 +1,8 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
 
-const host = 'localhost:3000';
-const protocol = 'http';
+const host = 'api.ticketshop.mixify.ga';
+const protocol = 'https';
 
 @Injectable({
   providedIn: 'root',
@@ -11,8 +11,36 @@ export class BackendService {
   constructor(private http: HttpClient) {}
 
   getMovies(callback) {
-    this.http.get<any>(`${protocol}://${host}/movies`).subscribe((data) => {
-      callback(data.data);
-    });
+    this.fetch("GET", "movies", callback);
+  }
+  getMovie(id: number, callback){
+    this.fetch("GET", `movies/${id}`, callback);
+  }
+  getMovieScreenings(callback){
+    this.fetch("GET", "movie-screenings", callback);
+  }
+  createCart(callback){
+    this.fetch("POST","carts", callback, {});
+  }
+  getCart(id: number, callback){
+
+    this.fetch("GET", `carts/${id}`, callback);
+  }
+
+  private fetch(requestMethod: string, endpoint: string, callback, httpbody?: any ){
+
+
+
+    if(requestMethod === "GET"){
+
+      this.http.get<any>(`${protocol}://${host}/${endpoint}`).subscribe((data) => {
+        callback(data.data);
+      });
+
+    } else if( requestMethod === "POST"){
+      this.http.post<any>(`${protocol}://${host}/${endpoint}`,httpbody).subscribe((data) => {
+        callback(data.data);
+      });
+    }    
   }
 }
