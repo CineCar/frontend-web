64c68c3cb98e54379dd1a4850472093e54a45a8f
Sebastian Meyne <sebastian.meyne@gmail.com>
2020-09-12 21:02:41 +0200

    implemented SoC in backend.service

--- src/app/services/backend.service.ts
+++ src/app/services/backend.service.ts
@@ -1,5 +1,6 @@
 import { Injectable, OnInit } from '@angular/core';
 import { HttpClient } from '@angular/common/http';
+import { Cart, Movie, MovieScreening } from 'com.cinecar.objects';
 
 const host = 'api.ticketshop.mixify.ga';
 const protocol = 'https';
@@ -8,39 +9,54 @@ const protocol = 'https';
   providedIn: 'root',
 })
 export class BackendService {
-  constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) { }
 
   getMovies(callback) {
-    this.fetch("GET", "movies", callback);
+    this.fetch("GET", "movies", (movies) => {
+      callback(<Movie[]>movies);
+    });
   }
-  getMovie(id: number, callback){
-    this.fetch("GET", `movies/${id}`, callback);
+  getMovie(id: number, callback) {
+    this.fetch("GET", `movies/${id}`, (movie) => {
+      callback(<Movie>movie);
+    });
   }
-  getMovieScreenings(callback){
-    this.fetch("GET", "movie-screenings", callback);
+  getMovieScreenings(callback) {
+    this.fetch("GET", "movie-screenings", (movieScreening) => {
+      callback(<MovieScreening>movieScreening);
+    });
   }
-  createCart(callback){
-    this.fetch("POST","carts", callback, {});
+  createCart(callback) {
+    this.fetch("POST", "carts", (cart) => {
+      callback(<Cart>cart);
+    }, {});
   }
-  getCart(id: number, callback){
+  getCart(id: number, callback) {
 
-    this.fetch("GET", `carts/${id}`, callback);
+    this.fetch("GET", `carts/${id}`, (cart) => {
+      callback(<Cart>cart);
+    });
+  }
+  addTicketToCart(id: number, movieScreeningId: number, callback) {
+    this.fetch("POST", `carts/${id}`, (cart) => {
+      callback(<Cart>cart);
+    }, { movieScreeningId: movieScreeningId })
   }
 
-  private fetch(requestMethod: string, endpoint: string, callback, httpbody?: any ){
+  private fetch(requestMethod: string, endpoint: string, callback, httpbody?: any) {
 
 
 
-    if(requestMethod === "GET"){
+    if (requestMethod === "GET") {
 
       this.http.get<any>(`${protocol}://${host}/${endpoint}`).subscribe((data) => {
         callback(data.data);
       });
 
-    } else if( requestMethod === "POST"){
-      this.http.post<any>(`${protocol}://${host}/${endpoint}`,httpbody).subscribe((data) => {
+    } else if (requestMethod === "POST") {
+      this.http.post<any>(`${protocol}://${host}/${endpoint}`, httpbody).subscribe((data) => {
         callback(data.data);
       });
-    }    
+    }
   }
 }
