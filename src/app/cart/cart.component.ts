e56d4b18634357bbe074fbf28c84707dd1472f9e
Sebastian Meyne <sebastian.meyne@gmail.com>
2020-09-13 17:38:21 +0200

    added checkout form

--- src/app/cart/cart.component.ts
+++ src/app/cart/cart.component.ts
@@ -3,6 +3,7 @@ import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
 import { Cart, Ticket } from 'com.cinecar.objects';
 import { CartService } from '../services/cart.service';
+import { FormBuilder } from "@angular/forms";
 
 @Component({
   selector: 'app-cart',
@@ -15,7 +16,18 @@ export class CartComponent implements OnInit {
   public tickets: Array<Ticket>;
   public total: number; 
 
-  constructor(private http: HttpClient) { }
+  public checkOutForm;
+
+  constructor(private http: HttpClient, private cartService: CartService, formBuilder: FormBuilder ) { 
+
+    this.checkOutForm = formBuilder.group({
+      firstname:"",
+      lastname:"",
+      email:"",
+      creditCard:"",
+      pin:""
+    })
+  }
 
   ngOnInit(): void {
     this.cartService = new CartService(this.http);
